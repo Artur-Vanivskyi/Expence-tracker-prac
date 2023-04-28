07b0cd57c4220087a8320dc1754d37e4182bebf5
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-27 20:59:49 -0400

    button can delete the transaction on click

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,11 @@
-import React,{ useContext } from "react";
+import React, { useContext } from "react";
 
 import { GlobalContext } from "../context/GlobalState";
 
 function Transaction({ transaction }) {
   const sign = transaction.amount < 0 ? "-" : "+";
-  
+
+  const { deleteTransaction } = useContext(GlobalContext);
 
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
@@ -12,7 +13,7 @@ function Transaction({ transaction }) {
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
     </li>
   );
 }
