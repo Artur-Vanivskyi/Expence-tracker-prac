0587f7cbbca66a54e298f31ded153470e00fb927
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-04-27 20:26:00 -0400

    created context API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpences from './components/IncomeExpences';
+import TransactionList from './components/TransactionList';
+import Addtransaction from './components/Addtransaction';
 
 
 function App() {
   return (
    <div>
     <Header />
+    <div className="container">
+      <Balance />
+      <IncomeExpences />
+      <TransactionList />
+      <Addtransaction />
+    </div>
    </div>
   );
 }
